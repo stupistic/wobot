cf66835d1c507a0c7b9b0da937aa7c041c97d6d6
stupistic <abhisheknaagar1998@gmail.com>
2022-07-13 14:25:23 +0530

    Project Completed

---
+++ wobot/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignIn from './component/Sign-in';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn/>
+    </div>
+  );
+}
+
+export default App;
